34bcba74c1b16ce479df00337c9f73a92c5d16cb
Alisa Vo <alisa.l.vo@ou.edu>
2019-12-01 17:03:10 -0600

    hoo

---
+++ Script.js
@@ -0,0 +1,11 @@
+// JavaScript source code
+const tabs = document.querySelectorAll('[data-tab-target')
+const tabContents = documen.querySelectorAll('[data-tab-content]')
+
+tabs.forEach(tab => {
+    tab.addEventListener('click', () => {
+        const target = document.querySelector(tab.dataset.tabTarget)
+        tabContents.forEach(tabContent => { tabContent.classList.remove('active') })
+        target.classList.add('active')
+    })
+})
